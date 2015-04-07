fd2b5444505dfb3508bd5d47580e674972170d29
Dan Schlauch <dschlauch@gmail.com>
2015-04-07 16:59:46 -0400

    Merge with bioconductor compatibility

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -8,13 +8,14 @@
 #' @export
 #' @return Summary description of panda S4 object
 #' @examples
+#' \donttest{
 #' data(pandaToyData)
 #' panda.res <- panda(pandaToyData$motif,
 #'            pandaToyData$expression,pandaToyData$ppi,hamming=.001,progress=TRUE)
-#' summary(panda.res)
+#' summary(panda.res)}
 summary.panda <- function(object, ...){
-  l <- list(coregNet=dim(object@coregNet),regNet=dim(object@regNet),coopNet=dim(object@coopNet))
-  cat("PANDA network for", nrow(object@coregNet),"genes and",nrow(object@coopNet)," transcription factors.")
+    l <- list(coregNet=dim(object@coregNet),regNet=dim(object@regNet),coopNet=dim(object@coopNet))
+    message("PANDA network for", nrow(object@coregNet),"genes and",nrow(object@coopNet)," transcription factors.")
 }
 #' print.panda
 #'
@@ -26,24 +27,25 @@ summary.panda <- function(object, ...){
 #' @export
 #' @return Summary description of panda S4 object
 #' @examples
+#' \donttest{
 #' data(pandaToyData)
 #' panda.res <- panda(pandaToyData$motif,
 #'            pandaToyData$expression,pandaToyData$ppi,hamming=.001,progress=TRUE)
-#' print(panda.res)
+#' print(panda.res)}
 print.panda <- function(x, ...){
-  l <- list(coregNet=dim(x@coregNet),regNet=dim(x@regNet),coopNet=dim(x@coopNet))
-  cat("PANDA network for", nrow(x@coregNet),"genes and",nrow(x@coopNet),"transcription factors.")
-  cat("\n\nSlots:\n")
-  cat(slotNames(x)[1],"\t: Regulatory network of",nrow(x@coopNet)," transcription factors to", nrow(x@coregNet),"genes.\n")
-  cat(slotNames(x)[2],": Co-regulation network of", nrow(x@coregNet),"genes.\n")
-  cat(slotNames(x)[3],"\t: Cooperative network of", nrow(x@coopNet),"transcription factors.\n\n")
-  numEdges <- sum(x@regNet!=0)
-  cat("Regulatory graph contains ",numEdges,"edges.\n")
-  if (numEdges==nrow(x@regNet)*ncol(x@regNet)){
-    cat("Regulatory graph is complete.")
-  } else {
-    cat("Regulatory graph is not complete.")
-  }
+    l <- list(coregNet=dim(x@coregNet),regNet=dim(x@regNet),coopNet=dim(x@coopNet))
+    message("PANDA network for", nrow(x@coregNet),"genes and",nrow(x@coopNet),"transcription factors.")
+    message("\nSlots:")
+    message(slotNames(x)[1],"\t: Regulatory network of",nrow(x@coopNet)," transcription factors to", nrow(x@coregNet),"genes.")
+    message(slotNames(x)[2],": Co-regulation network of", nrow(x@coregNet),"genes.")
+    message(slotNames(x)[3],"\t: Cooperative network of", nrow(x@coopNet),"transcription factors.\n")
+    numEdges <- sum(x@regNet!=0)
+    message("Regulatory graph contains ",numEdges,"edges.")
+    if (numEdges==nrow(x@regNet)*ncol(x@regNet)){
+        message("Regulatory graph is complete.")
+    } else {
+        message("Regulatory graph is not complete.")
+    }
 }
 #' Plot.panda
 #'
@@ -55,14 +57,16 @@ print.panda <- function(x, ...){
 #' @export
 #' @return Plot of the distribution of edge weights in the regulatory network.
 #' @examples
+#' \donttest{
 #' data(pandaToyData)
 #' panda.res <- panda(pandaToyData$motif,
 #'            pandaToyData$expression,pandaToyData$ppi,hamming=.001,progress=TRUE)
 #' plot(panda.res)
+#' }
 plot.panda <- function(x, ...){
-  cat("PANDA network for", nrow(x@coregNet),"genes and",nrow(x@coopNet)," transcription factors.\n")
-  cat("Mean edge weight = ", mean(x@regNet),"\n")
-  cat("Min edge weight = ", min(x@regNet),"\n")
-  cat("Max edge weight = ", max(x@regNet),"\n")
-  hist(x@regNet, main="Distribution of edge weights")
+    message("PANDA network for", nrow(x@coregNet),"genes and",nrow(x@coopNet)," transcription factors.")
+    message("Mean edge weight = ", mean(x@regNet))
+    message("Min edge weight = ", min(x@regNet))
+    message("Max edge weight = ", max(x@regNet))
+    hist(x@regNet, main="Distribution of edge weights")
 }
